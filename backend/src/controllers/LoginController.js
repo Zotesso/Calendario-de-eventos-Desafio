72b0f41783bc171f60f02757c0f6af22c3d9e08b
Zotesso <siriothart@gmail.com>
2020-09-27 21:20:11 -0300

    created login controller

--- backend/src/controllers/LoginController.js
+++ backend/src/controllers/LoginController.js
@@ -1,10 +1,30 @@
 const validatePassword = require('../utils/validatePassword');
+const bcrypt = require('bcrypt');
+const verifyUserExistence = require('../../src/utils/verifyUserExistence');
 
 module.exports = {
     async login(request, response,next){
         try{
-        userData =  request.body;
-        validatePassword(userData.password);
+            const userData =  request.body;
+
+            if(validatePassword(userData.password)){
+                const savedUser = await verifyUserExistence(userData.username);
+                if(savedUser){
+                    bcrypt
+                        .compare(userData.password ,savedUser.password)
+                        .then((result) => {
+                            if(result){
+                                response.json({
+                                    result,
+                                    message: 'Logando ...'
+                                }); 
+                            }else{
+                                next(new Error('invalid login'));
+                            }
+                        });
+                    }
+                }
+
         }catch(error){
             next(error);
         }
